efa3a3addcd13744c8db88f105470103860020f2
Paul Maliakel <paulbalu93@gmail.com>
2021-06-04 14:08:59 +0200

    adds delete router

--- src/services/accommodation/index.js
+++ src/services/accommodation/index.js
@@ -7,7 +7,6 @@ accommodationsRouter.get('/', async (req, res) => {
 	const accommodation = await accommodationModel.find({});
 	res.status(200).send({ accommodations: [] });
 });
-accommodationsRouter.get('/', async (req, res) => {});
 
 accommodationsRouter.get('/:id', async (req, res) => {});
 
@@ -16,14 +15,21 @@ accommodationsRouter.post('/', async (req, res) => {
 
 	if (!description || !name || !maxGuests || !city) throw new Error('Invalid data');
 
-	const accommodation = new ProductModel({ description, price });
+	const accommodations = new accomodationModel({ description, name, maxGuests, city });
 	await product.save();
 
-	res.status(201).send(product);
+	res.status(201).send(accommodations);
 });
 
 accommodationsRouter.put('/:id', async (req, res) => {});
 
-accommodationsRouter.delete('/', async (req, res) => {});
+accommodationsRouter.delete('/', async (req, res) => {
+	try {
+		await accomodationModal.findByIdAndDelete(req.params.id);
+		res.status(204).send();
+	} catch (error) {
+		next(error);
+	}
+});
 
 export default accommodationsRouter;
