7b002a728d17905c84d05280daa5809b288d5dce
Paul Maliakel <paulbalu93@gmail.com>
2021-06-04 13:48:29 +0200

    adds routers

--- src/services/accommodation/index.js
+++ src/services/accommodation/index.js
@@ -1,5 +1,32 @@
 import express from 'express';
+import accommodationModel from '../../models/accommodation/index.js'
 
 const accommodationRouter = Router();
+
+accommodationRouter.get("/", async (req, res)=> {
+
+res.status(200).send({ accommodations: []})
+})
+accommodationRouter.get("/", async (req, res)=> { 
+
+}
+
+accommodationRouter.get("/:id", async (req, res)=> {
+
+}
+
+accommodationRouter.post("/", async (req, res)=> {
+
+}
+
+accommodationRouter.put("/:id", async (req, res)=> {
+
+}
+
+accommodationRouter.delete("/", async (req, res)=> {
+
+}
+
+
 
 export default accommodationRouter;
